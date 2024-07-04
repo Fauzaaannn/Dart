e79806fc670f4e1f46c1dee36f7bb3b990a6f799
Fauzaaannn <fauzanabderrasheed@gmail.com>
2024-07-04 17:01:45 +0700

    Socket Programming

---
+++ Advance/socket_programming/bin/socket_programming.dart
@@ -0,0 +1,5 @@
+import 'package:socket_programming/socket_programming.dart' as socket_programming;
+
+void main(List<String> arguments) {
+  print('Hello world: ${socket_programming.calculate()}!');
+}
