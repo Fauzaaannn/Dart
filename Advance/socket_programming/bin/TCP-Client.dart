f313d90fa2669a70cb684fd952fba20f0ac81dc1
Fauzaaannn <fauzanabderrasheed@gmail.com>
2024-07-09 17:17:33 +0700

    Adding Notes

--- Advance/socket_programming/bin/TCP-Client.dart
+++ Advance/socket_programming/bin/TCP-Client.dart
@@ -14,5 +14,15 @@ void main(List<String> args) async
 
   Listening
   InternetAddress('127.0.0.1', IPv4):52419 = Hello World
+
+*/
+
+/* 
+  Notice how socket.write is synchronous and socket.close is asynchronous?
+  → 'send' is usually a very quick function and it returns very quickly
+  → where as 'close' can actually take some time to shut all that down.
   
+  you don't want to stall your application out
+  → Typically what happens is the data will go out to the TCP stack on the computer and the operating system
+    will handle the data push out so we don't have to worry about it
 */
